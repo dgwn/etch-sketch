612615289d859b188f22b3bd990c61373aa483f8
Daniel Gawne <danielgawne@gmail.com>
2017-09-20 15:04:53 -0400

    B&W drawing now works, prompt button added to change dimensions

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,15 @@ for (i = 0; i < gridSize; i++) {
     $('#container').append('<div class="pixel"></div>');
     };
   };
+
+function clearCreate() {
+  $('.pixelBlack').removeClass('pixelBlack');
+  gridSize = prompt("Please enter a number", "16")
+  
+};
+
+$(document).ready(function() {
+  $('.pixel').on('mouseenter', function() {
+    $(this).addClass('pixelBlack');
+  });
+});
