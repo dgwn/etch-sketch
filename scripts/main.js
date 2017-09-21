bb622a956d27665f709fbba94a1ce64bbb3a5a91
Daniel Gawne <danielgawne@gmail.com>
2017-09-20 23:10:58 -0400

    Allowed resolution to be changed without changing size of container

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,29 @@
-var gridSize = 16
+$(document).ready(function() {
+  createGrid(16);
+  mouseColor();
+});
 
-for (i = 0; i < gridSize; i++) {
-  for (j = 0; j < gridSize; j++) {
-    $('#container').append('<div class="pixel"></div>');
+
+function createGrid(gridSize) {
+  for (i = 0; i < gridSize; i++) {
+    for (j = 0; j < gridSize; j++) {
+      $('#container').append('<div class="pixel"></div>');
     };
   };
-
-function clearCreate() {
-  $('.pixelBlack').removeClass('pixelBlack');
-  gridSize = prompt("Please enter a number", "16")
-  
 };
 
-$(document).ready(function() {
+function mouseColor() {
   $('.pixel').on('mouseenter', function() {
     $(this).addClass('pixelBlack');
   });
-});
+};
+
+function clearCreate() {
+  $('.pixel').remove();
+  var gridSize = prompt("Please enter a number", "16")
+  createGrid(gridSize);
+  mouseColor();
+  $('.pixel').width((800 / gridSize)-1);
+  $('.pixel').height((800  / gridSize)-1);
+
+};
